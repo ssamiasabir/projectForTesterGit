4d7326774554a129ff52a62e1cf99b92a5d89f3e
Samia sabir <ssamiasabir13@gmail.com>
2023-12-26 06:58:33 -0500

    step is added

--- src/projectForTesterGit/gitNewClass.java
+++ src/projectForTesterGit/gitNewClass.java
@@ -5,6 +5,7 @@ public class gitNewClass {
 	public static void main(String[] args) {
 		System.out.println("10406 Ready to RUN");
 		System.out.println("10408 Ready to RUN");
+		System.out.println("105508 Ready to RUN"); // new step is added 
 
 
 	}
