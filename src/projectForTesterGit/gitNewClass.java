57fc965d4e4b2dc7ea1687ecaf02d3dfb0224566
Samia sabir <ssamiasabir13@gmail.com>
2023-12-26 06:56:04 -0500

    step is removed

--- src/projectForTesterGit/gitNewClass.java
+++ src/projectForTesterGit/gitNewClass.java
@@ -4,7 +4,6 @@ public class gitNewClass {
 
 	public static void main(String[] args) {
 		System.out.println("10406 Ready to RUN");
-		System.out.println("10407 Ready to RUN");
 		System.out.println("10408 Ready to RUN");
 
 
