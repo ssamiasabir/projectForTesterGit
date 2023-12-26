8c4daadbd22548632d744d2da977c0e78a4ac8ad
Samia sabir <ssamiasabir13@gmail.com>
2023-12-26 06:12:23 -0500

    New class is added for test--Ready for RUN

---
+++ src/projectForTesterGit/gitNewClass.java
@@ -0,0 +1,13 @@
+package projectForTesterGit;
+
+public class gitNewClass {
+
+	public static void main(String[] args) {
+		System.out.println("10406 Ready to RUN");
+		System.out.println("10407 Ready to RUN");
+		System.out.println("10408 Ready to RUN");
+
+
+	}
+
+}
