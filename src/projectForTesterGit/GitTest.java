35fd27ea5041dc359c3d30b8bc2618393b40dea7
Samia sabir <ssamiasabir13@gmail.com>
2023-12-26 06:09:22 -0500

    Check for new code

--- src/projectForTesterGit/GitTest.java
+++ src/projectForTesterGit/GitTest.java
@@ -9,6 +9,13 @@ System.out.println("Changed is made");
 System.out.println("new updates made Test case 10400 ready to RUN");
 
 System.out.println("10401 Ready to RUN");
+System.out.println("10402 Ready to RUN");
+System.out.println("10403 Ready to RUN");
+System.out.println("10404 Ready to RUN");
+System.out.println("10405 Ready to RUN");
+
+
+
 	}
 
 }
