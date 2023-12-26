addc41d9a3366222c2c3cefe500cddfe76dd83e6
Samia sabir <ssamiasabir13@gmail.com>
2023-12-26 05:46:29 -0500

    Changed is made

--- src/projectForTesterGit/GitTest.java
+++ src/projectForTesterGit/GitTest.java
@@ -5,6 +5,10 @@ public class GitTest {
 	public static void main(String[] args) {
 System.out.println("This is Git test class");
 System.out.println("Changed is made");
+
+System.out.println("new updates made Test case 10400 ready to RUN");
+
+System.out.println("10401 Ready to RUN");
 	}
 
 }
